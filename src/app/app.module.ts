d401c0a347d5b445332ed9322d0ba772bdd60da8
sigiltenebrae <ck6202@rit.edu>
2022-08-03 17:28:51 -0400

    Initial commit with material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DeckListComponent } from './deck-list/deck-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeckListComponent
   ],
   imports: [
-    BrowserModule
+    NgModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
